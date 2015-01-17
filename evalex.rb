d8fb62dff3ce71c0839a2aebeb0601eae875cec2
Kuniaki IGARASHI <igaiga@gmail.com>
2015-01-17 13:30:38 +0900

    Add first test

--- evalex.rb
+++ evalex.rb
@@ -1,11 +1,21 @@
+class EvalEx
+  def run(input)
+    #...
+    output = 1
+  end
+  private
+  def parse
+  end
+end
+
 require "test-unit"
 
 class EvalExTest < Test::Unit::TestCase
   test "evalex" do
     assert do
       input = "4*5"
-      output = "20"
-      EvalEx.new.foo(input) == output
+      output = 1
+      EvalEx.new.run(input) == output
     end
   end
 end
